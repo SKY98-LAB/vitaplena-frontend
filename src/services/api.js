8c727308bd6ccf88ed2daa7ad64b1a3538a34488
SKY98 LAB <sky98lab.dev@gmail.com>
2026-08-02 22:49:08 -0500

    Configure API environment variables

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',baseURL: 'https://vitaplena-backend.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Agregar token a cada petición
